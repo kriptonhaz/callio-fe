77104e68116390b175167e1c3a8f84482a5f7442
kriptonhaz <kriptonhaz@gmail.com>
2026-01-01 23:06:22 +0700

    chore: Set callio-app instances to 1.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'callio-app',
       port: 3000,
-      instances: 'max',
+      instances: 1,
       exec_mode: 'cluster',
       script: './dist/server/server.js',
       interpreter: 'bun',
